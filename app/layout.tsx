e2c7a148bc99be9f35643397fbc177b089eb97bd
Mustafa <mustafa@changejar.in>
2025-07-26 13:46:33 +0530

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,4 @@
-import { Inter, Ubuntu_Mono } from 'next/font/google'
 import './globals.css'
-
-export const metadata = {
-  title: 'Mustafa Shakir',
-  description: 'I am a software engineer with experience in designing and developing distributed systems and cloud-based solutions.',
-  authors: [{ name: "Mustafa Shakir", url: "shakirmustafa.com" }],
-  keywords: ["Software Engineer", "Potato"],
-  creator: "Mustafa Shakir",
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: "https://shakirmustafa.com",
-    title: "Mustafa Shakir",
-    description: "I am a software engineer with experience in designing and developing distributed systems and cloud-based solutions.",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: "https://shakirmustafa.com/site.webmanifest"
-}
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: "--inter"
-})
-
-const mono = Ubuntu_Mono({
-  subsets: ['latin'],
-  variable: "--ubuntu-mono",
-  weight: "700"
-})
 
 export default function RootLayout({
   children,
@@ -39,8 +6,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${inter.variable} ${mono.variable} dark:bg-black bg-zinc-50 dark:text-gray-50 flex h-full flex-col`}>
-      <body className='font-inter'>{children}</body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,5 @@
-import Poem from "@/components/poem";
-
-const contact = {
-  Mail: 'mailto:hello@shakirmustafa.com',
-  Linkedin: 'https://www.linkedin.com/in/otatopotato',
-  Telegram: 'https://otatopotato.t.me',
-  Github: 'https://github.com/RtiM0',
-  Twitter: 'https://twitter.com/RtiM0',
-}
-
 export default function Home() {
   return (
-    <div className="fixed inset-0 flex justify-center sm:px-8">
-      <div className="flex w-full max-w-7xl lg:px-8">
-        <div className="grid grid-cols-2 gap-10 items-center bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20 overflow-auto">
-          <div className="col-span-2 md:col-span-1 lg:pl-32 p-10 h-1/2 tracking-tight text-zinc-800 dark:text-zinc-100">
-            <p className="text-lg">Hey! I&apos;m Mustafa,</p>
-            <h1 className="text-6xl font-bold">Software Engineer</h1>
-            <p className="pt-10">Contact Me:</p>
-            <ul className="list-disc pl-5">
-              {
-                Object.entries(contact)
-                  .map(
-                    ([key, value], i) =>
-                      <li key={i}><a href={value} className="text-blue-600 hover:text-blue-900 dark:text-teal-300 dark:hover:text-teal-600">{key}</a></li>
-                  )
-              }
-            </ul>
-          </div>
-          <div className="col-span-2 md:col-span-1 p-10 text-zinc-800 dark:text-zinc-100">
-            <Poem />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>BUILD HERE</>
   )
 }
